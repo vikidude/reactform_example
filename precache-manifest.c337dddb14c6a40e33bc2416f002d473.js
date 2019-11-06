203a284a0e6c7d5d142ab0813ef80e50b5334a51
Vigneshwaran E <80500@KSLND6.thehindu.co.in>
2019-11-06 17:24:37 +0530

    Updates

---
+++ precache-manifest.c337dddb14c6a40e33bc2416f002d473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4144e2a201d2e6d4da88cdd934b000c8",
+    "url": "/reactform_example/index.html"
+  },
+  {
+    "revision": "22bf263ee1e90affdc2b",
+    "url": "/reactform_example/static/css/main.901b9b6a.chunk.css"
+  },
+  {
+    "revision": "5dc4fd82a755907a0146",
+    "url": "/reactform_example/static/js/2.34c0d298.chunk.js"
+  },
+  {
+    "revision": "22bf263ee1e90affdc2b",
+    "url": "/reactform_example/static/js/main.a21fb4e6.chunk.js"
+  },
+  {
+    "revision": "0739c78f776b503fad86",
+    "url": "/reactform_example/static/js/runtime-main.e8b2e8e6.js"
+  }
+]);
