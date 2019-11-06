6e314f0ed83dc2bc47b6e0a0be39bbfef319ac4d
Vigneshwaran E <80500@KSLND6.thehindu.co.in>
2019-11-06 17:11:14 +0530

    build file

---
+++ build/precache-manifest.d9638b634ef29076f3332085cae6885d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd77f93ee6130b19c5faa2f5472b7c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ad19b7a1feeae1ff371",
+    "url": "/static/css/main.901b9b6a.chunk.css"
+  },
+  {
+    "revision": "cbbbbbafe4c2a2cc63dd",
+    "url": "/static/js/2.464aab9e.chunk.js"
+  },
+  {
+    "revision": "1ad19b7a1feeae1ff371",
+    "url": "/static/js/main.d83f98d5.chunk.js"
+  },
+  {
+    "revision": "d757f56bdf17f1a60ccd",
+    "url": "/static/js/runtime-main.0fc188fb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
